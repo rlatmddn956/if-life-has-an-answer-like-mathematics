b7a3bdde694c89f7b82d825f9b2d1d9f90319450
rlatmddn <rlatmddn956@gmail.com>
2025-12-16 10:49:09 +0900

    v0.3 If life has an answer like mathematics // ì이건 한계인데ㅠㅠ

---
+++ src/Favor.java
@@ -0,0 +1,14 @@
+public class Favor {
+    private double favorable;
+
+    public Favor() {
+    }
+
+    public Favor(double favorable) {
+        this.favorable = favorable;
+    }
+
+    public double getFavorable() {
+        return favorable;
+    }
+}
--- src/Model5.java
+++ src/Model5.java
@@ -1,5 +1,16 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.util.ArrayList;
+
 public class Model5 {
-    public static void main(String[] args) {
+    public static void select(int n, int r){
+        //함수 select 정의
+        ArrayList<Favor> population = new ArrayList();  // 사람의 호감도 리스트
+        for(int i=0;i<n;i++){
+            double favorable =(double)(Math.random()); //0 ~ 1
+            population.add(new Favor(favorable));
+        }
+
+        int candidate =
 
     }
 }
